05016ef61a6039bc8bad86f334ed5ec10232c7ee
AmjadWebDev <amjadwebdev@gmail.com>
2020-09-17 14:36:05 +0200

    create pages and components files

---
+++ wasel/src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./homepage.styles.scss";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <div className="menu-directory">
+        <div className="menu-item">
+          <div className="content">
+            <h1>food</h1>
+            <span></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Homepage;
